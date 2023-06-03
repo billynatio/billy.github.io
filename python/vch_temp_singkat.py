52fb46e3e31678a42d0fccc74bbdaa6fa1b037fd
billynatio <natiobilly@gmail.com>
2023-06-03 23:32:13 +0700

    up

--- python/vch_temp_singkat.py
+++ python/vch_temp_singkat.py
@@ -7,76 +7,80 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from google.oauth2 import service_account
 
+CLIENT_SECRET_FILE = "credentials.json"
+SCOPES = ["https://www.googleapis.com/auth/drive",
+          "https://www.googleapis.com/auth/documents"]
+
+# Service API
+service_drive = Create_Service(CLIENT_SECRET_FILE, "drive", "v3", "https://www.googleapis.com/auth/drive")
+service_doc = Create_Service(CLIENT_SECRET_FILE, "docs", "v1", ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/documents"])
+
+
 def vch_txt(pdf_path: str, txt_path: str, nama_voucher, kode_voucher, harga, digipos, jumlah_voucher, waktu):
     try:
-        # Membuat metadata file PDF
+        # Create PDF file metadata
         file_metadata = {
             "name": os.path.basename(pdf_path),
             "parents": ["13-DRLgEBsHh5BEX2OQtq53wqdErut8YA"],
-            "mimeType": "application/pdf",
+            "mimeType": "application/pdf"
         }
 
-        # Unggah file PDF ke Google Drive
+        # Upload PDF file to Google Drive
         media = MediaFileUpload(pdf_path, mimetype="application/pdf")
-        pdf_file = (
-            service_drive.files()
-            .create(body=file_metadata, media_body=media, fields="id")
-            .execute()
-        )
+        pdf_file = service_drive.files().create(body=file_metadata, media_body=media, fields="id").execute()
         pdf_file_id = pdf_file.get("id")
 
-        # Membuat metadata file Google Docs
+        # Create Google Docs file metadata
         gdoc_metadata = {
             "name": os.path.splitext(os.path.basename(pdf_path))[0],
             "mimeType": "application/vnd.google-apps.document",
-            "parents": ["13-DRLgEBsHh5BEX2OQtq53wqdErut8YA"],
+            "parents": ["13-DRLgEBsHh5BEX2OQtq53wqdErut8YA"]
         }
 
-        # Mengonversi file PDF menjadi Google Docs
-        gdoc_file = (
-            service_drive.files().copy(fileId=pdf_file_id, body=gdoc_metadata).execute()
-        )
+        # Convert PDF file to Google Docs
+        gdoc_file = service_drive.files().copy(fileId=pdf_file_id, body=gdoc_metadata).execute()
         gdoc_file_id = gdoc_file.get("id")
 
-        # Unduh GDoc sebagai file teks
-        txt_data = service_drive.files().export_media(fileId=gdoc_file_id, mimeType='text/plain').execute()
+        # Export GDoc as plain text file
+        txt_data = service_drive.files().export_media(fileId=gdoc_file_id, mimeType="text/plain").execute()
 
-        with open(os.path.join(txt_path), 'wb') as f:
+        with open(os.path.join(txt_path), "wb") as f:
             f.write(txt_data)
 
-        # Baca file teks
-        with open(txt_path, 'r', encoding='utf-8') as file:
+        # Read the text file
+        with open(txt_path, "r", encoding="utf-8") as file:
             lines = file.readlines()
 
-        # Saring data
+        # Filter data
         filtered_lines = []
         for line in lines:
-            numbers = ''.join(filter(str.isdigit, line))
+            numbers = "".join(filter(str.isdigit, line))
             if len(numbers) == 17:
                 filtered_lines.append(numbers)
 
-        # Simpan data yang telah difilter ke dalam file sementara
+        # Save the filtered data to a temporary file
         temp_path = txt_path + ".temp"
-        with open(temp_path, 'w', encoding='utf-8') as file:
+        with open(temp_path, "w", encoding="utf-8") as file:
             for line in filtered_lines:
-                file.write(line + '\n')
+                file.write(line + "\n")
 
-        # Ganti file asli dengan file yang telah difilter
-        os.remove(txt_path)
-        os.rename(temp_path, txt_path)
-        
+        # Rename the temporary file with "filtered_" prefix
+        filtered_txt_path = "filtered_" + os.path.basename(txt_path)
+        os.rename(temp_path, filtered_txt_path)
+
         # Create the template using the filtered data
         template = "{}#{}#{}#{}#{}".format(
             kode_voucher,
             nama_voucher,
             "#".join(filtered_lines),
             harga,
-            waktu,
+            waktu
         )
 
         # Save the template to a file
         with open("template.txt", "w", encoding="utf-8") as file:
             file.write(template)
+            print("- Berhasil mendapatkan data dengan format voucher!")
 
         return gdoc_file_id
 
